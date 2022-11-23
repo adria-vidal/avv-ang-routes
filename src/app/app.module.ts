c4d8698a9cf2e3c867628936880d18ce35a9acfa
Adrià <adrivid98@gmail.com>
2022-11-23 10:59:38 +0100

    ejemplos de enrutado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuIzqComponent } from './menu-izq/menu-izq.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [NavbarComponent, MenuIzqComponent, MainComponent],
+  imports: [CommonModule],
+  exports: [NavbarComponent,MenuIzqComponent],
+})
+export class ElementsModule {}
